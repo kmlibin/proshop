2e2daa0a5e86901e8d5981ec9e807b8940f201e7
Kelli Libin <km.putnam2@gmail.com>
2023-07-27 11:45:53 -0600

    displayed ratings

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
@@ -14,6 +15,9 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
         </Card.Title>
       </Link>
+      <Card.Text as="div">
+        <Rating value= {product.rating} text = {`${product.numReviews} reviews`}/>
+      </Card.Text>
       <Card.Text as="h3">
         ${product.price}
       </Card.Text>
