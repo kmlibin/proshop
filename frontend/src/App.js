64ce55faa13c556b25871681edbe7ee7c08fe9ea
Kelli Libin <km.putnam2@gmail.com>
2023-07-27 11:26:59 -0600

    convert to react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import {Outlet} from 'react-router-dom'
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Container } from "react-bootstrap";
+
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>proshop</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
