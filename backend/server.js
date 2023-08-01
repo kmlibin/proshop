1167a1117e533f8b45c4162303b982a51262e118
Kelli Libin <km.putnam2@gmail.com>
2023-08-01 11:05:12 -0600

    moved routes to productRoutes file

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv'
-dotenv.config()
-import connectDB from './config/db.js'
-import products from './data/products.js'
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 //connect to MongoDB
-connectDB()
+connectDB();
 //create server
-const app = express()
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("API running");
+});
 
-app.get('/', (req, res) => {
-    res.send('API running')
-})
-
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((i) => i._id === req.params.id);
-    res.json(product)
-})
+app.use("/api/products", productRoutes);
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`)
-})
+  console.log(`server running on port ${port}`);
+});
