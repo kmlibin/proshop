de1fd03251be9ac7b0101c6a640bae46b9960340
Kelli Libin <km.putnam2@gmail.com>
2023-08-01 11:29:12 -0600

    move route code to controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,27 +1,11 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import {
+  getAllProducts,
+  getSingleProduct,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const { id: productId } = req.params;
-    const singleProduct = await Product.findById(productId);
-    if(singleProduct) {
-       res.status(200).json(singleProduct);  
-    }
-   res.status(404).json({msg: 'product not found'})
-  })
-);
-
+router.route("/").get(getAllProducts);
+router.route("/:id").get(getSingleProduct);
 export default router;
