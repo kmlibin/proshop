fb8d7ddcbfa0f13aa5d7f8c602d98a9c83a64273
Kelli Libin <km.putnam2@gmail.com>
2023-08-03 14:21:14 -0600

    implement userRoutes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from './routes/userRoutes.js'
 
 //connect to MongoDB
 connectDB();
@@ -15,10 +16,12 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
-app.use(notFound);
-app.use(errorHandler)
+app.use("/api/users", userRoutes);
 
-const port = process.env.PORT;
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
