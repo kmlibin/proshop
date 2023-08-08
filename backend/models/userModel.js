1ef24ae87185d8971db228b577869a23b8668202
Kelli Libin <km.putnam2@gmail.com>
2023-08-08 16:05:07 -0600

    pre function to hash passwords

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,6 +30,17 @@ userSchema.methods.matchPassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
+//saving to the database, this gets done before it's saved
+userSchema.pre('save', async function (next) {
+  //if it's not modified, we call the next piece of middlware
+  if (!this.isModified('password')) {
+    next()
+  }
+  //create a hashed password and save it to the db. grabbed password before it's saved
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
