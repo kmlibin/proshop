c5bd0ac4b62fca1321f86d53303e3a7e2f80051b
Kelli Libin <km.putnam2@gmail.com>
2023-08-09 14:51:53 -0600

    logout to post request, changed /login to /auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -15,8 +15,8 @@ import {
 const router = express.Router();
 
 router.route("/").post(registerUser).get(protect, admin, getAllUsers);
-router.route("/logout").get(logoutUser);
-router.route("/login").post(authUser);
+router.route("/logout").post(logoutUser);
+router.route("/auth").post(authUser);
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 router.route("/:id").delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser);
 
