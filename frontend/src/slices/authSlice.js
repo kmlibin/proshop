f25d9664fe9115bd74a423c21cdd8e771772a522
Kelli Libin <km.putnam2@gmail.com>
2023-08-09 14:52:57 -0600

    added logout action

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -2,6 +2,8 @@
 
 import { createSlice } from "@reduxjs/toolkit";
 
+//deeals with local state
+
 const initialState = {
   userInfo: localStorage.getItem("userInfo")
     ? JSON.parse(localStorage.getItem("userInfo"))
@@ -12,12 +14,17 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
+    //call when we get cookie back. we store the user info , NOT token
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
       localStorage.setItem("userInfo", JSON.stringify(action.payload));
     },
+    logout: (state, action) => {
+      state.userInfo = null;
+      localStorage.removeItem("userInfo");
+    },
   },
 });
 
-export const { setCredentials } = authSlice.actions
+export const { setCredentials, logout } = authSlice.actions;
 export default authSlice.reducer;
