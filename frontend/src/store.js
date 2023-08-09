263684c8ae741fc7b160b5e1c588759ba8447648
Kelli Libin <km.putnam2@gmail.com>
2023-08-09 11:13:11 -0600

    added authslice to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,14 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice.js";
 import cartSliceReducer from "./slices/cartSlice.js";
+import authSliceReducer from './slices/authSlice.js';
 
 const store = configureStore({
   reducer: {
-    //don't have to bring other reducers in because we are using apiSlice.inject endpoints
+    //don't have to bring other api reducers in because we are using apiSlice.inject endpoints
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
+    auth: authSliceReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
