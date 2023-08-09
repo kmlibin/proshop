020bfcd5f6e60dbe5fffa09614f7d374a5366233
Kelli Libin <km.putnam2@gmail.com>
2023-08-09 14:53:22 -0600

    added logout action

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,18 +1,27 @@
 import { USERS_URL } from "../constants.js";
 import { apiSlice } from "./apiSlice.js";
 
+
+//deals with api calls
+
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    //post request, so mutation
+    //post request, so mutation. sends request to backend and gets cookie. then when we get data back, we call "set credentials" in authslice
     login: builder.mutation({
       //name and password send in data
       query: (data) => ({
-        url: USERS_URL / auth,
+        url: `${USERS_URL}/auth`,
         method: "POST",
         body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
       }),
     }),
   }),
 });
 
-export const { useLoginMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
