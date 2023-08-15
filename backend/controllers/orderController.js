6eb0e9e5695061c581c9b2e117bab75f04f061a5
Kelli Libin <km.putnam2@gmail.com>
2023-08-15 11:50:46 -0600

    create order, get by id,  get user orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,20 +5,7 @@ import Order from "../models/orderModel.js";
 //@route   POST /api/orders
 //@access  private
 const createOrder = asyncHandler(async (req, res) => {
-  res.send("create order");
-});
-
-//@desc    Get logged in users orders
-//@route   GET /api/orders/myorders
-//@access  private
-const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("get users orders");
-});
-
-//@desc    Get order by ID
-//@route   GET /api/orders/:id
-//@access  private/admin
-const getOrderById = asyncHandler(async (req, res) => {
+  console.log(req.body);
   const {
     orderItems,
     shippingAddress,
@@ -32,11 +19,13 @@ const getOrderById = asyncHandler(async (req, res) => {
   if (orderItems && orderItems.length === 0) {
     res.status(400).json({ msg: "no order items" });
   } else {
-    //order model - order items has a product category that isn't passed with the frontend. we have to add it, which is why we mapped. it's 
+    //order model - order items has a product category that isn't passed with the frontend. we have to add it, which is why we mapped. it's
     //linked with id.
     const order = new Order({
       orderItems: orderItems.map((o) => ({
-        ...o, product: o._id, _id: undefined
+        ...o,
+        product: o._id,
+        _id: undefined,
       })),
       user: req.user._id,
       shippingAddress,
@@ -46,7 +35,32 @@ const getOrderById = asyncHandler(async (req, res) => {
       shippingPrice,
       totalPrice,
     });
+
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
   }
+});
+
+//@desc    Get logged in users orders
+//@route   GET /api/orders/myorders
+//@access  private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
+});
+
+//@desc    Get order by ID
+//@route   GET /api/orders/:id
+//@access  private/admin
+const getOrderById = asyncHandler(async (req, res) => {
+    //find by id, and also populate the user's name and email (from the 'user' schema)
+  const order = await Order.findById(req.params.id).populate('user', 'name email');
+  if(order) {
+   res.status(200).json(order) 
+  }else {
+    res.status(404).json({msg: 'order not found'})
+  }
+  
 });
 
 //@desc    Update Order to paid
