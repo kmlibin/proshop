e6721b98b9ad3f332e4305cf98179878b04f8151
Kelli Libin <km.putnam2@gmail.com>
2023-08-16 11:58:14 -0600

    app.get for paypal

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,17 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 //connect to MongoDB
 connectDB();
 //create server
 const app = express();
 //body parser middleware
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API running");
@@ -23,7 +23,12 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
-app.use('/api/orders', orderRoutes)
+app.use("/api/orders", orderRoutes);
+
+//set up paypal
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
 app.use(notFound);
 app.use(errorHandler);
