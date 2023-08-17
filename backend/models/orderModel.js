e3253a34862170e2daec5f753355378db71c176e
Kelli Libin <km.putnam2@gmail.com>
2023-08-17 12:09:19 -0600

    typo - qty to price

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -4,7 +4,7 @@ const singleItem = new mongoose.Schema({
   name: { type: String, required: true },
   qty: { type: Number, required: true },
   image: { type: String, required: true },
-  qty: { type: Number, required: true },
+  price: { type: Number, required: true },
   product: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
