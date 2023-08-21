07161107a35b63d3325894cef24fa53f7882d040
Kelli Libin <km.putnam2@gmail.com>
2023-08-21 15:37:08 -0600

    route for editing

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,13 @@ import {
   createProduct,
   getAllProducts,
   getSingleProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getSingleProduct);
+router.route("/:id").get(getSingleProduct).put(protect, admin, updateProduct);
 
 export default router;
