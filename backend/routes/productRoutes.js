c302b99b5924441a5bda9056ce9a00d9df6a72ce
Kelli Libin <km.putnam2@gmail.com>
2023-08-21 14:39:46 -0600

    add route for admin product adding

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
+  createProduct,
   getAllProducts,
   getSingleProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").get(getAllProducts);
+router.route("/").get(getAllProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getSingleProduct);
 
 export default router;
