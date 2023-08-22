14da0099a3635caa2df04af920202f0036c000b6
Kelli Libin <km.putnam2@gmail.com>
2023-08-22 10:27:15 -0600

    added upload route

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
@@ -7,7 +8,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-
+import uploadRoutes from "./routes/uploadRoutes.js";
 //connect to MongoDB
 connectDB();
 //create server
@@ -24,11 +25,16 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 //set up paypal
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+//set uploads folder as a static folder
+const __dirname = path.resolve(); //sets __Dirname to current directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
