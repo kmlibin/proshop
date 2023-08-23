a900a6ecec6f9df4872ac61464596c2c5ccef94a
Kelli Libin <km.putnam2@gmail.com>
2023-08-23 11:53:39 -0600

    create and show search bar

---
+++ frontend/src/components/SearchBox.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useParams, useNavigate } from "react-router-dom";
+
+const SearchBox = () => {
+  const navigate = useNavigate();
+  const { keyword: urlKeyword } = useParams();
+  const [keyword, setKeyword] = useState(urlKeyword || "");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      navigate(`/search/${keyword}`);
+      setKeyword('')
+    } else {
+      navigate("/");
+    }
+  };
+  return (
+    <Form onSubmit={handleSubmit} className="d-flex">
+      <Form.Control
+        type="text"
+        name="q"
+        onChange={(e) => setKeyword(e.target.value)}
+        value={keyword}
+        placeholder="Search Products..."
+        className="mr-sm-2 ml-sm-5"
+      ></Form.Control>
+      <Button type="submit" variant="outline-light" className="p-2 mx-2">
+        Search
+      </Button>
+    </Form>
+  );
+};
+
+export default SearchBox;
